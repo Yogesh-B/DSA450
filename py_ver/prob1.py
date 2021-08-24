baf155c9046efc19c00ddf07655253a17eb935f7
yogesh-b <yogeshbavishi199@gmail.com>
2021-08-24 21:28:57 +0530

    #Reverse the array

--- py_ver/prob1.py
+++ py_ver/prob1.py
@@ -2,19 +2,20 @@
 #https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 
 
-#assume that user will give correct input and no error happens
-in_arr = input("enter number separated by space:").strip()
-arr = [int(num) for num in in_arr.split(" ")]
 
-#set first value
-min_val = arr[0]
-max_val = arr[0]
+def reverseArr(arr1):
+  full_len = len(arr1)
+  half_len = int(len(arr1)/2)
+  for i in range(half_len):
+    tmp_char = arr1[i]
+    arr1[i] = arr1[full_len - i - 1]
+    arr1[full_len - i - 1] = tmp_char
+  return arr1
 
 
-for i in arr :
-  if i < min_val :
-    min_val = i
-  if i > max_val :
-    max_val = i
-
-print("min - ",min_val," // max - ",max_val)
+if __name__ == "__main__":
+  #assume that user will give correct input and no error happens
+  in_arr = [int(x) for x in input("Enter numbers separated by space : ").strip().split(" ")]
+  print("original : ",in_arr)
+  rev_arr = reverseArr(in_arr)
+  print("Reverse : ",rev_arr)
